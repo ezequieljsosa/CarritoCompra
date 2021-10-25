a8aacb31fba48f8658e81ad5455b89c206068817
Ezequiel Sosa <ezequieljsosa@gmail.com>
2021-10-25 16:33:56 +0000

    Working commit

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 import ar.edu.utn.CarritoCompra;
 import ar.edu.utn.DescuentoPorcentaje;
 import ar.edu.utn.Producto;
-import ar.edu.utn.CompraProducto;
+import ar.edu.utn.ItemCarrito;
 
 
 class Main {
@@ -10,12 +10,12 @@ class Main {
 
     Producto p1 = new Producto("cosito del coso");
     p1.setPrecio(100);
-    CompraProducto cp1 = new CompraProducto( 2 ,p1);
+    ItemCarrito cp1 = new ItemCarrito( 2 ,p1);
     
     CarritoCompra cc = new CarritoCompra();
     cc.addProducto(cp1);
-    //DescuentoPorcentaje descuento = new DescuentoPorcentaje(0.10);
-    Descuento descuento = new DescuentoFijo(180);
+    DescuentoPorcentaje descuento = new DescuentoPorcentaje(0.10);
+    
 
     System.out.println(cc.precio(descuento));
     
--- ar/edu/utn/CarritoCompra.java
+++ ar/edu/utn/CarritoCompra.java
@@ -2,16 +2,16 @@ package  ar.edu.utn;
 
 public class CarritoCompra {
 
-  private CompraProducto compraProducto;
+  private ItemCarrito item;
 
   // Por ahora lo simplificamos a un solo articulo
-  public void addProducto(CompraProducto cp) {
-    this.compraProducto = cp;
+  public void addProducto(ItemCarrito cp) {
+    this.item = cp;
   }
 
   public int precio(Descuento descuento){
-    return compraProducto.precio() - 
-            descuento.descuento(compraProducto.precio());
+    return item.precio() - 
+            descuento.descuento(item.precio());
   }
 
 }
